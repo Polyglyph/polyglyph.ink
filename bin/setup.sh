72dad6192dd98bb170506c5f9d18d46fe8647a2e
Jeppe Klitgaard <jeppe@dapj.dk>
2021-05-15 22:04:24 +0100

    fix poetry install

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Install Poetry
-curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python -
+pip install poetry
 
 # Install packages
 poetry install --no-dev
